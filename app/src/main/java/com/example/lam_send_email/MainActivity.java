c90af3044a7b46797edc32e547a3223c4ddc2a13
jorge <jorgefdc@outlook.com>
2023-10-17 20:14:29 +0100

    Data ready to send to second activity

--- app/src/main/java/com/example/lam_send_email/MainActivity.java
+++ app/src/main/java/com/example/lam_send_email/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.lam_send_email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String MESSAGE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(this, MainActivity2.class);
+        TextInputEditText fromInput = findViewById(R.id.fromInput);
+        TextInputEditText toInput = findViewById(R.id.toInput);
+        TextInputEditText messageInput = findViewById(R.id.messageInput);
+
+        MESSAGE = R.string.from + " " + fromInput + "\n" + R.string.to + " " + toInput + "\n" +
+                R.string.message + " " + messageInput;
+
+        startActivity(intent);
     }
 }
